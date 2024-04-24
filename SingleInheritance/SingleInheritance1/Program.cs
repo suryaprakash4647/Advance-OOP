8b4460a84d4fd53e248025296b7cfea91e045bb0
suryaprakash4647 <166098622+suryaprakash4647@users.noreply.github.com>
2024-04-25 01:39:10 +0530

    Single Inheritance

---
+++ SingleInheritance/SingleInheritance1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SingleInheritance1;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Objects
+        PersonalDetails personal=new PersonalDetails("Vijay","Surya",9876543210,"sdfghgfd@gmail.com",new DateTime(2000,10,10),Gender.Male);
+        StudentDetails student=new StudentDetails(personal.Name,personal.FatherName,personal.Phone,personal.Mail,personal.DOB,personal.Gender, "10","science",2023);
+        //Method call 
+        student.ShowStudentInfo();
+    }
+    
+}
